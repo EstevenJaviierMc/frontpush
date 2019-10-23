621d37a36939ab98b010894017f59b4dd1d1eb22
Esteven Javier Mc <EstevenJaviier@gmail.com>
2019-10-23 09:30:25 -0500

    13102019

--- src/app/service/invitacion.service.ts
+++ src/app/service/invitacion.service.ts
@@ -13,17 +13,17 @@ export class InvitacionService {
   constructor(private http: HttpClient) { }
 
   newInvitacion(invitacion: any): Observable<any> {
-    return this.http.post(endpoint + 'new-invitacion', invitacion, httpOptions)
+    return this.http.post(endpoint + 'invitations', invitacion, httpOptions)
       .pipe(map(data => data));
   }
 
   getInvitaciones(): Observable<any> {
-    return this.http.get(endpoint + 'invitaciones', httpOptions)
+    return this.http.get(endpoint + 'invitations', httpOptions)
       .pipe(map(data => data));
   }
 
   deleteInvitacion(id): Observable<any> {
-    return this.http.delete(endpoint + 'invitacion/' + id, httpOptions)
+    return this.http.delete(endpoint + 'invitations/' + id, httpOptions)
       .pipe(map(data => data));
   }
 }
