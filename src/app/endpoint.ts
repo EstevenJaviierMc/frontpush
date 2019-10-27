87b2d1a00f927432d698c692f5a1f06eb1b64c2e
Esteven Javier Mc <EstevenJaviier@gmail.com>
2019-10-27 11:57:25 -0500

    deploy

--- src/app/endpoint.ts
+++ src/app/endpoint.ts
@@ -1,6 +1,6 @@
 import { HttpHeaders } from "@angular/common/http";
-const urlBackend = '162.243.162.72:8000';
-const endpoint = "http://" + urlBackend + "/api/v1/";
+const urlBackend = 'cfcpdf.herokuapp.com';
+const endpoint = "https://" + urlBackend + "/api/v1/";
 const httpOptions = {
     headers: new HttpHeaders({
         'Content-Type': 'application/json',
