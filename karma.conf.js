5b66f9bea222194efea5264bf02584e0e7550c84
Esteven Javier Mc <EstevenJaviier@gmail.com>
2020-04-24 20:51:10 -0500

    24042020

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/cfcPalabradeFe'),
+      dir: require('path').join(__dirname, './coverage/hellomundo'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
